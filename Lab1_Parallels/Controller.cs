240f2a64a6d97ca24a9527549e29ddf299f63eb3
Ростислав Лещенко <rostishkaleschenko2006@gmail.com>
2026-02-25 12:18:01 +0200

    result

---
+++ Lab1_Parallels/Controller.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1_Parallels
+{
+    public class Controller
+    {
+        private volatile bool isRunning = false;
+        public bool IsRunning => isRunning;
+        public Controller() { }
+
+        public void Start()
+        {
+            Thread.Sleep(3000);
+            isRunning = true;
+        }
+    }
+}
---
+++ Lab1_Parallels/Program.cs
@@ -0,0 +1,33 @@
+﻿using Lab1_Parallels;
+using System;
+namespace Lav
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            Random rnd = new();
+            Controller controller = new();
+            Func<int,double, Controller, double> f = (i, p,  c) =>
+            {
+                double result = 0;
+                int count = 0;
+                while (!c.IsRunning)
+                {
+                    result += p;
+                    count++;
+                }
+                Console.WriteLine($"Thread with number {i} and step {p}" +
+                    $" executed with result {result} and count of additions {count}");
+                return 0;
+            };
+            for (int i = 1; i <= n; i++)
+            {
+                int localI = i;
+                new Thread(() => f(localI, rnd.NextDouble(),controller)).Start();
+            }
+            new Thread(()=>controller.Start()).Start();
+        }
+    }
+}
